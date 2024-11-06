56c1cf344eb5a5be162f29722bdf69a97f35e727
Akilesh Sethu <asethu19@gmail.com>
2024-11-06 09:41:03 +0800

    run parallel and benchstat

--- fsm_test.go
+++ fsm_test.go
@@ -1,9 +1,12 @@
 package fsm_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/bento01dev/fsm"
+	csfsm "github.com/cocoonspace/fsm"
+	llfsm "github.com/looplab/fsm"
 )
 
 const (
@@ -24,6 +27,24 @@ const (
 )
 
 func BenchmarkFSM(b *testing.B) {
+	f := llfsm.NewFSM(
+		"foo",
+		llfsm.Events{
+			{Name: "foo", Src: []string{"foo"}, Dst: "bar"},
+			{Name: "bar", Src: []string{"bar"}, Dst: "foo"},
+		},
+		llfsm.Callbacks{},
+	)
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			f.Event(context.Background(), "foo")
+			f.Event(context.Background(), "bar")
+		}
+	})
+}
+
+func BenchmarkMyFSM(b *testing.B) {
 	states := []fsm.State{STATE_1, STATE_2, STATE_3, STATE_4, STATE_5}
 	events := []fsm.Event{EVENT_1, EVENT_2, EVENT_3, EVENT_4, EVENT_5, EVENT_6}
 	transitions := []fsm.Transition{
@@ -34,6 +55,10 @@ func BenchmarkFSM(b *testing.B) {
 					Event: EVENT_2,
 					Next:  STATE_2,
 				},
+				{
+					Event: EVENT_1,
+					Next:  STATE_1,
+				},
 			},
 		},
 		{
@@ -43,6 +68,10 @@ func BenchmarkFSM(b *testing.B) {
 					Event: EVENT_1,
 					Next:  STATE_1,
 				},
+				{
+					Event: EVENT_2,
+					Next:  STATE_2,
+				},
 			},
 		},
 	}
@@ -51,12 +80,55 @@ func BenchmarkFSM(b *testing.B) {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if _, err := f.Transition(EVENT_2); err != nil {
-			b.Fatal(err)
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			if _, err := f.Transition(EVENT_2); err != nil {
+				b.Fatal(err)
+			}
+			if _, err := f.Transition(EVENT_1); err != nil {
+				b.Fatal(err)
+			}
 		}
-		if _, err := f.Transition(EVENT_1); err != nil {
-			b.Fatal(err)
+	})
+}
+
+const (
+	StateFoo csfsm.State = iota
+	StateBar
+)
+
+const (
+	EventFoo csfsm.Event = iota
+	EventBar
+)
+
+func BenchmarkCocoonSpaceFSM(b *testing.B) {
+	f := csfsm.New(StateFoo)
+	f.Transition(csfsm.On(EventFoo), csfsm.Src(StateFoo), csfsm.Dst(StateBar))
+	f.Transition(csfsm.On(EventBar), csfsm.Src(StateBar), csfsm.Dst(StateFoo))
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			f.Event(EventFoo)
+			f.Event(EventBar)
 		}
-	}
+	})
+}
+
+func BenchmarkLooplabFSM(b *testing.B) {
+	f := llfsm.NewFSM(
+		"foo",
+		llfsm.Events{
+			{Name: "foo", Src: []string{"foo"}, Dst: "bar"},
+			{Name: "bar", Src: []string{"bar"}, Dst: "foo"},
+		},
+		llfsm.Callbacks{},
+	)
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			f.Event(context.Background(), "foo")
+			f.Event(context.Background(), "bar")
+		}
+	})
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/bento01dev/fsm
 go 1.22.5
 
 require golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
+
+require (
+	github.com/cocoonspace/fsm v1.0.1 // indirect
+	github.com/looplab/fsm v1.0.2 // indirect
+)
